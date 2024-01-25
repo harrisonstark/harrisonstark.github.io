c04054dcafd6e0b8c9b5edef2cf4d39c392c280f
harrisonstark <harrisonstarkshs@gmail.com>
2024-01-24 21:00:27 -0800

    add in navbar toggle, update some text, and overall improve the look

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// page load
+
+window.onbeforeunload = function () {
+    window.scrollTo(0, 0);
+  }
+
+// nav button
+
+const toggleBtn = document.querySelector('.navbar-toggle');
+const navbar = document.querySelector('.navbar');
+const content = document.querySelector('.content');
+
+toggleBtn.addEventListener('click', () => {
+  navbar.classList.toggle('raise');
+  content.classList.toggle('raise');
+});
