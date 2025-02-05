8d4cd473c283e75b0e74ef71e3f20176da0eed5e
harrisonstark <harrisonstarkshs@gmail.com>
2025-02-04 23:17:22 -0800

    luka goals

---
+++ files/Luka_Goals_2025.py
@@ -0,0 +1,19 @@
+luka_goals = [
+    'Gym consistent', 
+    'Job', 
+    'Make frens',
+    'Go to national parks (3+',
+    'Learn more piano',
+    'Build robo car',
+    'Learn French cuisine',
+    'Get abs',
+    'Learn to drive a manual',
+    'Decorate Luka room'
+]
+
+def main():
+    goal_index = int(input("Please insert index of Luka Goal\n"))
+    print(luka_goals[goal_index])
+
+if __name__ == '__main__':
+    main()
